d31874b3e8eab22199cf99a110780071098a9bb5
ou.yun <ou.yun@tci-cn.co.jp>
2019-03-07 19:00:39 +0900

    fixed bug

--- background.js
+++ background.js
@@ -59,6 +59,10 @@ function setPriorty() {
 }
 
 function setMemberList() {
+  function split( val ) {
+    return val.split( /,\s*/ );
+  }
+  
   function extractLast( term ) {
     return split( term ).pop();
   }
